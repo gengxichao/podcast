76b833a991b4509e7eea2ccc03f85cbd424213e2
Geng Xichao <gengxichao@vip.qq.com>
2016-12-17 10:23:38 +0800

    finish signin

--- pages/signin/signin.js
+++ pages/signin/signin.js
@@ -3,7 +3,10 @@ Page({
     tip:"",
     username: "",
     password:"",
+    nickname:"",
     gender:"",
+    male:"",
+    female:"",
     email:"",
     telphone:"",
     date:"",
@@ -17,21 +20,66 @@ Page({
   },
   formBindsubmit:function(e){
     var that = this
-    if(e.detail.value.username.length==0||e.detail.value.password.length==0||e.detail.value.gender.length==0||e.detail.value.email.length==0||e.detail.value.telphone.length==0)
+    //邮箱正则表达式验证
+    function checkemail(email_addr){
+      var regex = /^([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g;
+      if (regex.test(email_addr))
+      {
+          return true
+      }
+      else{
+        return false
+      }
+    }
+    if(e.detail.value.username.length==0||e.detail.value.password.length==0||e.detail.value.nickname.length==0||e.detail.value.gender.length==0||e.detail.value.email.length==0||e.detail.value.telphone.length==0)
     {
-
       wx.showModal({
         title: '信息不完整',
         content: '所有字段都是必填项喔~',
         showCancel: false
       })
-
-    }else{
+    }
+    else if(e.detail.value.password != e.detail.value.repassword){
+        wx.showModal({
+        title: '确认密码不相同',
+        content: '请重新输入确认密码~',
+        showCancel: false
+      })
+    }
+    else if(!checkemail(e.detail.value.email)){
+        wx.showModal({
+        title: '邮箱格式不正确',
+        content: '请重新输入邮箱~',
+        showCancel: false
+      })
+    }
+    else if(e.detail.value.telphone.length != 11){
+        wx.showModal({
+        title: '手机号码位数不正确',
+        content: '请重新输入手机号~',
+        showCancel: false
+      })
+    }
+    else{
+      that.setData({
+        username:e.detail.value.username,
+        password:e.detail.value.password,
+        email:e.detail.value.email,
+        date:e.detail.value.date,
+        telphone:e.detail.value.telphone,
+      })
+      if(e.detail.value.gender == "male"){
+        male:"true"
+        female:"false"
+      }
+      else{
+        male:"false"
+        female:"true"       
+      }
       var sendData = '{ \n \
         "Request":"Register", \n \
         "UserName":"' + that.data.username + '", \n \
         "Password":"' + that.data.password + '", \n \
-        "ComfirmedPassword":"' + that.data.password + '", \n \
         "NickName":"' + that.data.password + '", \n \
         "DateOfBirth":"' + that.data.password + '", \n \
         "Gender":"' + that.data.gender + '", \n \
